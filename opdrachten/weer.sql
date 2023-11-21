171d783013f7c7760799e0b0fc148ec389cc81c5
sem <34904@ma-web.nl>
2023-11-21 15:44:23 +0100

    new

--- opdrachten/weer.sql
+++ opdrachten/weer.sql
@@ -12,7 +12,7 @@ datum DATE NOT NULL,
 aantalGraden DECIMAL NOT NULL,
 windKracht INT NOT NULL,
 regenInMilimeters DECIMAL NOT NULL,
-plaats VARCHAR(45) NOT NULL,
+plaats VARCHAR(45) NOT NULL
 
 PRIMARY KEY (idWeersomstandighedenPerDag),
 UNIQUE INDEX idWeersomstandighedenPerDag_UNIQUE (idWeersomstandighedenPerDag ASC) VISIBLE
